d78443574d3b67c7e9eb6a5932dace82fe80f8af
Prithvi <69208476+germanebeing@users.noreply.github.com>
2025-06-30 02:28:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/fieldassist-moon-beverages-insights/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
